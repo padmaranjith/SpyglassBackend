ec50b2fdea04724261e3f8a90f531de5f82197cd
padma <padmaranjith28@gmail.com>
2023-06-20 17:18:54 -0400

    Created spring backend

---
+++ src/main/java/com/skillstorm/SpyglassApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpyglassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpyglassApplication.class, args);
+	}
+
+}
