4d79242d0cd816e68d6e9b95357254335b48e3ab
padma <padmaranjith28@gmail.com>
2023-06-29 22:49:23 -0400

    added cross origin at application layer

--- src/main/java/com/skillstorm/SpyglassApplication.java
+++ src/main/java/com/skillstorm/SpyglassApplication.java
@@ -2,8 +2,10 @@ package com.skillstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://spyglass-project3.s3-website-us-east-1.amazonaws.com/")
 public class SpyglassApplication {
 
 	public static void main(String[] args) {
